22522af85539023e270d97af34c88864e9224110
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2018-12-04 15:24:56 +0200

    Remove auction post view for lot results

--- openprocurement/auctions/appraisal/views/auction.py
+++ openprocurement/auctions/appraisal/views/auction.py
@@ -49,21 +49,3 @@ class AppraisalAuctionAuctionResource(AuctionAuctionResource):
             self.LOGGER.info('Report auction results',
                              extra=context_unpack(self.request, {'MESSAGE_ID': 'auction_auction_post'}))
             return {'data': self.request.validated['auction'].serialize(self.request.validated['auction'].status)}
-
-    @json_view(content_type="application/json", permission='auction', validators=(validate_auction_auction_data))
-    def post(self):
-        """Report auction results for lot.
-        """
-        apply_patch(self.request, save=False, src=self.request.validated['auction_src'])
-        auction = self.request.validated['auction']
-        adapter = self.request.registry.getAdapter(auction, IAuctionManager)
-        if all([i.auctionPeriod and i.auctionPeriod.endDate for i in auction.lots if i.numberOfBids > 1 and i.status == 'active']):
-            cleanup_bids_for_cancelled_lots(auction)
-            invalidate_bids_under_threshold(auction)
-            if any([i.status == 'active' for i in auction.bids]):
-                self.request.content_configurator.start_awarding()
-            else:
-                adapter.pendify_auction_status('unsuccessful')
-        if save_auction(self.request):
-            self.LOGGER.info('Report auction results', extra=context_unpack(self.request, {'MESSAGE_ID': 'auction_lot_auction_post'}))
-            return {'data': self.request.validated['auction'].serialize(self.request.validated['auction'].status)}
