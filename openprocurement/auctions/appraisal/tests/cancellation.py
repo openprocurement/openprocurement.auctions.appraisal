c949b724ae9c47ce7aff7228934985bc47f587d1
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2019-01-30 18:26:57 +0200

    Expand tests

--- openprocurement/auctions/appraisal/tests/cancellation.py
+++ openprocurement/auctions/appraisal/tests/cancellation.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import unittest
+import mock
+
+from datetime import timedelta
+from openprocurement.auctions.core.utils import get_now
+
+from openprocurement.auctions.core.tests.base import snitch
 
 from openprocurement.auctions.appraisal.tests.base import (
-    BaseAppraisalAuctionWebTest, test_bids,
+    BaseAppraisalAuctionWebTest, test_bids
 )
 from openprocurement.auctions.core.tests.cancellation import (
     AuctionCancellationResourceTestMixin,
     AuctionCancellationDocumentResourceTestMixin
 )
+
+from openprocurement.auctions.appraisal.tests.blanks.cancellation_blanks import check_cancellation_document_type_required
 
 
 class AppraisalAuctionCancellationResourceTest(BaseAppraisalAuctionWebTest,
@@ -16,11 +24,10 @@ class AppraisalAuctionCancellationResourceTest(BaseAppraisalAuctionWebTest,
     initial_bids = test_bids
 
 
-class AppraisalAuctionCancellationDocumentResourceTest(BaseAppraisalAuctionWebTest,
-                                                     AuctionCancellationDocumentResourceTestMixin):
+class BaseCancellationDocumentTest(BaseAppraisalAuctionWebTest):
 
     def setUp(self):
-        super(AppraisalAuctionCancellationDocumentResourceTest, self).setUp()
+        super(BaseCancellationDocumentTest, self).setUp()
         # Create cancellation
         response = self.app.post_json('/auctions/{}/cancellations?acc_token={}'.format(
             self.auction_id, self.auction_token
@@ -28,11 +35,32 @@ class AppraisalAuctionCancellationDocumentResourceTest(BaseAppraisalAuctionWebTe
         cancellation = response.json['data']
         self.cancellation_id = cancellation['id']
 
+        self.patch_document_type_required_from = mock.patch(
+            'openprocurement.auctions.appraisal.models.DOCUMENT_TYPE_REQUIRED_FROM',
+            get_now() + timedelta(days=10)
+        )
+        self.mock_document_type_required_from = self.patch_document_type_required_from.start()
+
+    def tearDown(self):
+        self.patch_document_type_required_from.stop()
+
+
+class AppraisalAuctionCancellationDocumentResourceTest(BaseCancellationDocumentTest,
+                                                     AuctionCancellationDocumentResourceTestMixin):
+    pass
+
+
+class AppraisalAuctionCancellationWithDSDocumentResourceTest(BaseCancellationDocumentTest):
+    docservice = True
+
+    test_check_cancellation_document_type_required = snitch(check_cancellation_document_type_required)
+
 
 def suite():
     tests = unittest.TestSuite()
     tests.addTest(unittest.makeSuite(AppraisalAuctionCancellationResourceTest))
     tests.addTest(unittest.makeSuite(AppraisalAuctionCancellationDocumentResourceTest))
+    tests.addTest(unittest.makeSuite(AppraisalAuctionCancellationWithDSDocumentResourceTest))
     return tests
 
 
